35e895293d1931dea60c30473d08e84591d3bf92
Ángel Igareta <alu0100967111@ull.edu.es>
2018-04-21 12:13:58 -0700

    Cleaned step finished.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# socialmediadata
+# SocialMediaData
+
+Program desgined to clean a file and calculate it's corpus.
---
+++ src/SocialMediaParser.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * SocialMediaParser.java
+ *
+ * @author Ángel Igareta
+ * @author Miguel Jimenez 
+ * @version 1.0
+ * @since 21-04-2018
+ */
+
+/**
+ * Parser designed to clean a file and calculate it's vocabulary.
+ */
+public class SocialMediaParser {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			throw new IllegalArgumentException("You must specify the input txt");
+		}
+
+		String cleanedFile = cleanFile(args[0]);
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter("cleaned_"+ args[0]));
+		writer.write(cleanedFile);
+
+		writer.close();
+	}
+
+	/**
+	 * Clean a file passed by argument.
+	 * 
+	 * @param reader
+	 * @return
+	 * @throws IOException
+	 */
+	private static String cleanFile(String inputFile) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		String cleanedFile = "";
+
+		while (reader.ready()) {
+			cleanedFile += cleanLine(reader.readLine()) + System.lineSeparator();
+		}
+
+		reader.close();
+		return cleanedFile;
+	}
+
+	/**
+	 * Clean the line according to the characters we can find in the text.
+	 * 
+	 * @param readLine
+	 *          Line to clean
+	 * @return Cleaned Line
+	 */
+	private static String cleanLine(String readLine) {
+		/** REPLACES */
+		readLine = readLine.replaceAll("^ ", "");
+		readLine = readLine.replaceAll("‰Ûª", "'");
+		readLine = readLine.replaceAll("‰Û_", "%");
+		readLine = readLine.replaceAll(".@", "@");
+		readLine = readLine.replaceAll("@", " @");
+		readLine = readLine.replaceAll("&amp", "&");
+		readLine = readLine.replaceAll("&gt", ">");
+		readLine = readLine.replaceAll("&lt", "<");
+		readLine = readLine.replaceAll("‰ÛÒ", "-");
+		readLine = readLine.replaceAll("w/", "with");
+		readLine = readLine.replaceAll("\\s{2}\\s*", " ");
+
+		/** REMOVES */
+		readLine = readLine.replaceAll("‰Û", "");
+		readLine = readLine.replaceAll("‰ÛÏ", "");
+		readLine = readLine.replaceAll("åÊ", "");
+		readLine = readLine.replaceAll(":", "");
+
+		/** LINKS REMOVED */
+		readLine = readLine.replaceAll("http[^\\s]*", " ");
+
+		return readLine;
+	}
+}
